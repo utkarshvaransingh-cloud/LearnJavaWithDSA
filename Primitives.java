16b3e43d9441de917a0930166298fe740065bd9a
utkarshvaransingh-cloud <utkarshvaransingh@gmail.com>
2026-06-05 02:21:33 +0530

    learning basics input/output, data types primitive

---
+++ Primitives.java
@@ -0,0 +1,17 @@
+public class Primitives {
+
+    public static void main(String[] args) {
+        int rollno=64;
+        float marks=98.55f;
+        double largeDecimalNumbers=456234267.45678;
+        long largeInteger=456473575375375735L;
+        boolean check=false;
+
+        // Primitive -> no sub division 
+        // String is not primitive , String can be Divided in characters.
+        String name="Varan";
+        char letter='r';
+
+        Integer rno=43;
+    }
+}
